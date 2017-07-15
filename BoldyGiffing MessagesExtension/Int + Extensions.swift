7148ac9b1d2632feb1b0c0a1f4a475de38292f95
Aaron Williams <theadwilliams@gmail.com>
2017-07-14 18:18:28 -0700

    Add random int extension

---
+++ BoldyGiffing MessagesExtension/Int + Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Int + Extensions.swift
+//  BoldyGiffing MessagesExtension
+//
+//  Created by Aaron Williams on 2017-07-10.
+//  Copyright © 2017 SweetieApps. All rights reserved.
+//
+
+import Foundation
+
+public extension Int {
+    /**
+     * Returns a random integer between 0 and n-1.
+     */
+    public static func random(_ n: Int) -> Int {
+        return Int(arc4random_uniform(UInt32(n)))
+    }
+}
