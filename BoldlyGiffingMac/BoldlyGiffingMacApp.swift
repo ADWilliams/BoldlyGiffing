e88ae6b6cf1deb0c2b188527f1e24a2e8a69d057
Aaron Williams <aaron@sweetsoftware.email>
2022-04-05 13:38:26 -0700

    Add MacApp target to project

---
+++ BoldlyGiffingMac/BoldlyGiffingMacApp.swift
@@ -0,0 +1,18 @@
+//
+//  BoldlyGiffingMacApp.swift
+//  BoldlyGiffingMac
+//
+//  Created by Aaron Williams on 2022-03-31.
+//  Copyright © 2022 SweetieApps. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BoldlyGiffingMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoldlyGiffingMac/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BoldlyGiffingMac
+//
+//  Created by Aaron Williams on 2022-03-31.
+//  Copyright © 2022 SweetieApps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
