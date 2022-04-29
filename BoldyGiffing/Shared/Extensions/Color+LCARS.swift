9cca3af04523f4a57c0c0b7cbf008370519f13b8
Aaron Williams <aaron@sweetsoftware.email>
2022-04-29 14:32:07 -0700

    Add LCARSColors

---
+++ BoldyGiffing/Shared/Extensions/Color+LCARS.swift
@@ -0,0 +1,23 @@
+//
+//  Color+LCARS.swift
+//  BoldyGiffing MessagesExtension
+//
+//  Created by Aaron Williams on 2022-04-22.
+//  Copyright © 2022 SweetieApps. All rights reserved.
+//
+
+import SwiftUI
+
+enum LCARSColor {
+    static let almond = Color("LCARSAlmond")
+    static let gold = Color("LCARSGold")
+    static let lightBlue = Color("LCARSLightBlue")
+    static let blue = Color("LCARSBlue")
+    static let orange = Color("LCARSOrange")
+    static let red = Color("LCARSRed")
+    static let text = Color("LCARSText")
+    static let tomato = Color("LCARSTomato")
+    static let violet = Color("LCARSViolet")
+    static let white = Color("LCARSWhite")
+    static let yellow = Color("LCARSYellow")
+}
