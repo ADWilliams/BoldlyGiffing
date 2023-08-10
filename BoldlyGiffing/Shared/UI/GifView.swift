7941fb24c1ef5295e344e4b19aec51cf63d7d132
Aaron Williams <aaron@sweetsoftware.email>
2023-08-10 16:17:24 -0700

    Adjust background of gif view

--- BoldlyGiffing/Shared/UI/GifView.swift
+++ BoldlyGiffing/Shared/UI/GifView.swift
@@ -12,7 +12,6 @@ import SDWebImageSwiftUI
 struct GifView: View {
     let gif: Gif
     let isLarge: Bool
-    @State private var progress: Double = 1
     
     internal init(gif: Gif, isLarge: Bool = false) {
         self.gif = gif
@@ -21,15 +20,15 @@ struct GifView: View {
     
     var body: some View {
         AnimatedImage(url: isLarge ? gif.fullSizeURL : gif.thumbnailURL)
-                .placeholder(content: {
-                    Color(red: 0.26, green: 0.53, blue: 0.96).opacity( isLarge ? 0 : 0.8)
-                })
-                .transition(.fade)
-                .resizable()
-                .aspectRatio(1.2, contentMode: .fit)
-                .opacity(progress)
-                .cornerRadius(4)
-                .scaledToFill()
+            .transition(.fade)
+            .resizable()
+            .aspectRatio(1.2, contentMode: .fit)
+            .scaledToFill()
+            .background {
+                Color(red: 0.26, green: 0.53, blue: 0.96)
+                    .opacity( isLarge ? 0 : 0.6)
+            }
+            .cornerRadius(isLarge ? 16 : 4)
     }
     
 
